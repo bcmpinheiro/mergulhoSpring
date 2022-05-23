40d58b9ea4de23ec685ee95e889b02cd4f3efead
Barbara <bcmpinheiro@gmail.com>
2022-05-22 23:49:44 -0300

    implements method consult in client repository

--- src/main/java/com/algaworks/algalog/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.algaworks.algalog.controller;
 
 import com.algaworks.algalog.model.Client;
 import com.algaworks.algalog.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,15 @@ import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClientController {
-
-    @PersistenceContext
-    private EntityManager manager;
 
     @Autowired
     private ClientRepository clientRepository;
 
     @GetMapping("/clients")
     public List<Client> list() {
-        return manager.createQuery("from Client", Client.class)
-                .getResultList();
+        return clientRepository.findAll();
     }
 }
--- src/main/java/com/algaworks/algalog/repository/ClientRepository.java
+++ src/main/java/com/algaworks/algalog/repository/ClientRepository.java
@@ -4,6 +4,13 @@ import com.algaworks.algalog.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientRepository extends JpaRepository <Client, Long> {
+    List<Client> findByName (String name);
+    List<Client> findByNameContaining (String name);
+
+
+
 }
