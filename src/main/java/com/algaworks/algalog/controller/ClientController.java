063cb91850ae8eebbdd8a91aced9ad502fad78c0
Barbara <bcmpinheiro@gmail.com>
2022-05-22 23:14:11 -0300

    add repository and others class for project

--- src/main/java/com/algaworks/algalog/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/controller/ClientController.java
@@ -1,30 +1,28 @@
 package com.algaworks.algalog.controller;
 
 import com.algaworks.algalog.model.Client;
+import com.algaworks.algalog.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Autowired
+    private ClientRepository clientRepository;
+
     @GetMapping("/clients")
     public List<Client> list() {
-
-        var client01 = new Client();
-        client01.setId(1L);
-        client01.setName("Joao");
-        client01.setCellphone("11 91234-5678");
-        client01.setEmail("joao@email.com");
-
-        var client02 = new Client();
-        client02.setId(2L);
-        client02.setName("Maria");
-        client02.setCellphone("11 91234-5678");
-        client02.setEmail("maria@email.com");
-
-        return Arrays.asList(client01, client02);
+        return manager.createQuery("from Client", Client.class)
+                .getResultList();
     }
 }
--- src/main/java/com/algaworks/algalog/model/Client.java
+++ src/main/java/com/algaworks/algalog/model/Client.java
@@ -1,15 +1,29 @@
 package com.algaworks.algalog.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Client {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String cellphone;
+
     private String email;
 }
