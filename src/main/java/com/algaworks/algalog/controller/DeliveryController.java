6fa9209b2b1f55b8835e6e9b16cb2afd56622ebc
Barbara <bcmpinheiro@gmail.com>
2022-07-04 15:55:13 -0300

    add modelmapper in project

--- src/main/java/com/algaworks/algalog/controller/DeliveryController.java
+++ src/main/java/com/algaworks/algalog/controller/DeliveryController.java
@@ -4,8 +4,8 @@ import com.algaworks.algalog.domain.model.Delivery;
 import com.algaworks.algalog.domain.repository.DeliveryRepository;
 import com.algaworks.algalog.domain.service.DeliveryServiceRequest;
 import com.algaworks.algalog.model.DeliveryModel;
-import com.algaworks.algalog.model.ReceiverModel;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +20,7 @@ public class DeliveryController {
 
     private DeliveryRepository deliveryRepository;
     private DeliveryServiceRequest deliveryServiceRequest;
+    private ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -36,19 +37,21 @@ public class DeliveryController {
     public ResponseEntity<DeliveryModel> search(@PathVariable Long deliveryId) {
         return deliveryRepository.findById(deliveryId)
                 .map(delivery -> {
-                    DeliveryModel deliveryModel = new DeliveryModel();
-                    deliveryModel.setId(delivery.getId());
-                    deliveryModel.setNameClient(delivery.getClient().getName());
-                    deliveryModel.setReceiver(new ReceiverModel());
-                    deliveryModel.getReceiver().setName(delivery.getReceiver().getName());
-                    deliveryModel.getReceiver().setAdress(delivery.getReceiver().getAdress());
-                    deliveryModel.getReceiver().setNumber(delivery.getReceiver().getNumber());
-                    deliveryModel.getReceiver().setComplement(delivery.getReceiver().getComplement());
-                    deliveryModel.getReceiver().setDistrict(delivery.getReceiver().getDistrict());
-                    deliveryModel.setTax(delivery.getTax());
-                    deliveryModel.setStatus(delivery.getStatus());
-                    deliveryModel.setDateRequest(delivery.getDateRequest());
-                    deliveryModel.setDateFinish(delivery.getDateFinish());
+                    DeliveryModel deliveryModel = modelMapper.map(delivery, DeliveryModel.class);
+
+//                    DeliveryModel deliveryModel = new DeliveryModel();
+//                    deliveryModel.setId(delivery.getId());
+//                    deliveryModel.setNameClient(delivery.getClient().getName());
+//                    deliveryModel.setReceiver(new ReceiverModel());
+//                    deliveryModel.getReceiver().setName(delivery.getReceiver().getName());
+//                    deliveryModel.getReceiver().setAdress(delivery.getReceiver().getAdress());
+//                    deliveryModel.getReceiver().setNumber(delivery.getReceiver().getNumber());
+//                    deliveryModel.getReceiver().setComplement(delivery.getReceiver().getComplement());
+//                    deliveryModel.getReceiver().setDistrict(delivery.getReceiver().getDistrict());
+//                    deliveryModel.setTax(delivery.getTax());
+//                    deliveryModel.setStatus(delivery.getStatus());
+//                    deliveryModel.setDateRequest(delivery.getDateRequest());
+//                    deliveryModel.setDateFinish(delivery.getDateFinish());
 
                     return ResponseEntity.ok(deliveryModel);
                 })
